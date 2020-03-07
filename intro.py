a778992aa3fb18f5ddc64efc066f24c51b32abba
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-07 19:45:39 +0530

    initial commit

---
+++ intro.py
@@ -0,0 +1,9 @@
+print("hello python interpreter")
+# this is called variable
+# variable name can only contain letters,numbers and underscores
+# cant start with a number
+# cant use key words as variables
+# best practice to use lowercase    
+message = "hello world python"
+
+print(message)
